9eeaaa98e1d9f897f8887e21f760ffb33299467b
Dušan Simić <dusan.simic1810@gmail.com>
2017-10-07 13:04:37 +0200

    Dodavanje domaceg string
    Dodat je domaci string, nije jos zavrsen ali ovo je za pocetak.

---
+++ string/MojString.cs
@@ -0,0 +1,123 @@
+using System;
+
+namespace mojtipstring {
+	class MojString {
+		private char[] str;
+
+		private void Add(char c) {
+			if (str != null) {
+				char[] newStr = new char[GetLength() + 1];
+				for (int i = 0; i < GetLength(); i++) {
+					newStr[i] = str[i];
+				}
+				newStr[GetLength()] = c;
+				str = newStr;
+			}
+		}
+
+		public MojString(char[] niz)
+		{
+			if (niz != null) {
+				str = new char[niz.Length];
+				for (int i = 0; i < niz.Length; i++) {
+					str[i] = niz[i];
+				}
+			} else {
+				System.Console.WriteLine("Prosledjeni niz nije definisan!");
+			}
+		}
+		public int GetLength() {
+			if (str != null) {
+				return str.Length;
+			}
+			return -1;
+		}
+		public char[] GetArray() {
+			if (str != null) {
+				return str;
+			}
+			return new char[0];
+		}
+		public char GetCharAt(int i) {
+			return str[i];
+		}
+		public void SetCharAt(int i, char c) {
+			str[i] = c;
+		}
+		public int GetIndexOf(char c) {
+			for (int i = 0; i < GetLength(); i++) {
+				if (GetCharAt(i) == c) {
+					return i;
+				}
+			}
+			return -1;
+		}
+		public void Append(MojString s) {
+			char[] newStr = new char[GetLength() + s.GetLength()];
+			for (int i = 0; i < GetLength(); i++) {
+				newStr[i] = GetCharAt(i);
+			}
+			for (int i = GetLength(); i < GetLength() + s.GetLength(); i++) {
+				newStr[i] = s.GetCharAt(i - GetLength());
+			}
+			str = newStr;
+		}
+		public void Prepend(MojString s) {
+			char[] newStr = new char[GetLength() + s.GetLength()];
+			for (int i = 0; i < s.GetLength(); i++) {
+				SetCharAt(i, s.GetCharAt(i));
+			}
+			for (int i = s.GetLength(); i < GetLength() + s.GetLength(); i++) {
+				SetCharAt(i, GetCharAt(i - s.GetLength()));
+			}
+			str = newStr;
+			//s.Append(this);
+		}
+		public void Insert(int i, MojString s) {
+			char[] newStr = new char[GetLength() + s.GetLength()];
+			for (int j = 0; j < GetLength() + s.GetLength(); j++) {
+				if (j == i) {
+					for (; j < s.GetLength() + i; j++) {
+						newStr[j] = s.GetCharAt(j - i);
+					}
+					j--;
+				} else if (j < i) {
+					newStr[j] = GetCharAt(j);
+				} else {
+					newStr[j] = GetCharAt(j - s.GetLength());
+				}
+			}
+			str = newStr;
+		}
+		public MojString[] Split(char c) {
+			/*
+			 * TODO
+			 * Implementacija splita.
+			 * Podeliti string na vise stringova a da se ne koriste liste.
+			 */
+			return new MojString[0];
+		}
+		public MojString Substring(int i, int n) {
+			if (str != null) {
+				if (i >= 0 && i + n < GetLength()) {
+					MojString s = new MojString(new char[0]);
+					for (int j = i; j <= i + n; j++) {
+						s.Add(GetCharAt(j));
+					}
+					return s;
+				}
+			}
+			return new MojString(new char[0]);
+		}
+		public void Print(bool nl) {
+			if (str != null) {
+				for (int i = 0; i < GetLength(); i++) {
+					System.Console.Write("{0}", GetCharAt(i));
+				}
+				if (nl) {
+					System.Console.WriteLine();
+				}
+			}
+		}
+	}
+}
---
+++ string/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace mojtipstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] string1 = {'H', 'e', 'r', 'l', 'd'};
+            char[] string2 = {'l', 'l', 'o', ' ', 'w', 'o'};
+            MojString s1 = new MojString(string1);
+            MojString s2 = new MojString(string2);
+            
+            s1.Insert(2, s2);
+            s1.Print(true);
+            MojString s3 = s1.Substring(2, 3);
+            s3.Print(true);
+        }
+    }
+}
